fe156485a8ce5d2b982a748f36fbd6893fecda6e
puneet <psoraganvi01@gmail.com>
2022-08-12 22:59:40 +0530

    first commit.....

---
+++ src/test/java/first/NewTest.java
@@ -0,0 +1,23 @@
+package first;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hello");
+  }
+  @Test
+  public void a() {
+	  System.out.println("java");
+  }
+  @Test
+  public void b() {
+	  System.out.println("selenium");
+  }
+  @Test
+  public void c() {
+	  System.out.println("Paython");
+  }
+  
+}
